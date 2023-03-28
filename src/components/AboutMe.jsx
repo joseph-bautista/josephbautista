b0f8ffa54226cce8c049085e655f403718e0de4d
Joseph Bautista <40680817+joseph-bautista@users.noreply.github.com>
2023-03-28 08:08:14 +0800

    about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -18,7 +18,7 @@ const AboutMe = () => {
                     </h2>
                     <h6 id="job">Software Developer</h6>
                     <p className="expound">
-                        Hi, I am a software developer based in the Philippines. I have more than 5 years working in software development industry. I worked as Full Stack Web Application Developer for more than 4 years and, right now, I am working as Back End Engineer at <a href="https://xctuality.com" target="_blank"><strong>Xctuality Lte. Ptd.</strong></a>, a Singapore-based tech-start up, for more than a year until present. I believe that studying is a lifetime commitment, that's why during my free time I am exploring new things in tech such as blockchain technology, cryptocurrency and Artificial Intelligence.
+                        Hi, I am a software developer based in the Philippines. I have more than 5 years working in software development industry. I worked as Full Stack Web Application Developer for more than 4 years and, right now, I am working remotely as Back End Engineer at <a href="https://xctuality.com" target="_blank"><strong>Xctuality Lte. Ptd.</strong></a>, a Singapore-based tech-start up, for more than a year until present. I believe that studying is a lifetime commitment, that's why during my free time I am exploring new things in tech such as blockchain technology, cryptocurrency and Artificial Intelligence.
                     </p>
                     <p className="expound">
                         I am open for new opportunities that would need my services in software development. If you have one for me, please email me at <a href="mailto:josephreuben.bautista@gmail.com" target="_blank"><strong><em className="ref">josephreuben.bautista@gmail.com</em></strong></a>
